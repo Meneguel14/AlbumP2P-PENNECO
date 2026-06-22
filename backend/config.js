59e93d4b4dd6f83855d91257c669a6f8095e5562
PedroHPedroso <pedropedroso_adv@outlook.com>
2026-06-22 14:37:10 -0300

    Fix: Arquivo de configuração formatado para alterar de forma mais indicativa

--- backend/config.js
+++ backend/config.js
@@ -3,9 +3,9 @@
 // Altere apenas este arquivo para mudar o ID do aluno.
 // ============================================================
 
-const MEU_NODE_ID = process.env.NODE_ID || 'ALUNO-22';      // Seu identificador na rede
-const MINHA_FIGURINHA = process.env.NODE_FIGURINHA || 'FIG-22';    // Figurinha autoral (mesma numeração)
-const MEU_IP = process.env.NODE_IP || '26.57.185.95';       // IP atual da máquina na rede local
+const MEU_NODE_ID = process.env.NODE_ID || 'ALUNO-XX';      // Seu identificador na rede
+const MINHA_FIGURINHA = process.env.NODE_FIGURINHA || 'FIG-XX';    // Figurinha autoral (mesma numeração)
+const MEU_IP = process.env.NODE_IP || '0.0.0.0';       // IP atual da máquina na rede local
 const INITIAL_QUANTITY = 28;          // Distribuição inicial obrigatória
 const SEARCH_TTL = 7;                 // TTL inicial para buscas por inundação
 
