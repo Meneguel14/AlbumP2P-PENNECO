2acca571bc8a1be605e7c2c2ae503d1810913b0a
PedroHPedroso <pedropedroso_adv@outlook.com>
2026-06-21 22:39:37 -0300

    Fix: Colocado IP do radmin padrão para facilitar conexão

--- backend/config.js
+++ backend/config.js
@@ -5,7 +5,7 @@
 
 const MEU_NODE_ID = process.env.NODE_ID || 'ALUNO-22';      // Seu identificador na rede
 const MINHA_FIGURINHA = process.env.NODE_FIGURINHA || 'FIG-22';    // Figurinha autoral (mesma numeração)
-const MEU_IP = process.env.NODE_IP || '';       // IP atual da máquina na rede local
+const MEU_IP = process.env.NODE_IP || '26.57.185.95';       // IP atual da máquina na rede local
 const INITIAL_QUANTITY = 28;          // Distribuição inicial obrigatória
 const SEARCH_TTL = 7;                 // TTL inicial para buscas por inundação
 
